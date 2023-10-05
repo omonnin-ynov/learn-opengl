eb309571db4610c7e3185872e2b6d7692e196a25
omonnin-ynov <olivier.monnin@ynov.com>
2023-10-05 11:36:23 +0200

    Set independent vertex colors

--- Source2.cpp
+++ Source2.cpp
@@ -134,10 +134,11 @@ int main() {
     glDeleteShader(vertexShader);
 
     // Create VAO, VBO, EBO
-    float vertices[] = {// first triangle
-     -1.0f, 0.0f, 0.0f,  // top right
-     -0.5f, 0.0f, 0.0f,  // bottom right
-     -0.75f,  0.5f, 0.0f,  // top left 
+    float vertices[] = {
+        // positions         // colors
+         0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f,   // bottom right
+        -0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,   // bottom left
+         0.0f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f    // top 
     };
 
     float vertices2[] = {
@@ -159,8 +160,10 @@ int main() {
     glBindVertexArray(VAO[0]);
     glBindBuffer(GL_ARRAY_BUFFER, VBO[0]);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
 
     unsigned int EBO;
     glGenBuffers(1, &EBO);
@@ -197,8 +200,8 @@ int main() {
         glBindVertexArray(VAO[0]);
         glDrawArrays(GL_TRIANGLES, 0, 3);
         
-        glBindVertexArray(VAO[1]);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        // glBindVertexArray(VAO[1]);
+        // glDrawArrays(GL_TRIANGLES, 0, 3);
 
         glfwSwapBuffers(window);
         glfwPollEvents();
