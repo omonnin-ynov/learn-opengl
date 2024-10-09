2856dceede831eefe20be84d9caf1bcd7b0a53ad
omonnin-ynov <olivier.monnin@ynov.com>
2024-10-09 11:39:04 +0200

    viewspace shading

--- vertex.glsl
+++ vertex.glsl
@@ -6,15 +6,18 @@ layout (location = 1) in vec2 aTexCoord;
 uniform mat4 model;
 uniform mat4 view;
 uniform mat4 projection;
+uniform vec3 lightPos;
 
 out vec2 TexCoord;
 out vec3 Normal;
 out vec3 FragPos;
+out vec3 LightPos;
 
 void main()
 {
-    gl_Position = projection * view * model * vec4(aPos.x, aPos.y, aPos.z, 1); // see how we directly give a vec3 to vec4's constructor
+    gl_Position = projection * view * model * vec4(aPos, 1.0);
     TexCoord = aTexCoord;
-    FragPos = vec3(model * vec4(aPos, 1.0));
-    Normal = mat3(transpose(inverse(model))) * aNormal;
+    FragPos = vec3(view * model * vec4(aPos, 1.0));
+    Normal = mat3(transpose(inverse(view * model))) * aNormal;
+    LightPos = vec3(view * vec4(lightPos, 1.0)); // Transform world-space light position to view-space light position
 }
